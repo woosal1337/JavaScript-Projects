fccc8ed8153589c72953588a8e5e27c926676a88
Vusal Ismayilov <woosal@protonmail.com>
2021-03-18 00:34:27 +0300

    8.0

---
+++ filterProject/js/app.js
@@ -0,0 +1,8 @@
+const allButton = document.getElementsByClassName("btn btn-outline-secondary btn-black text-uppercase filter-btn m-2");
+
+for (let i = 0; i < allButton.length; i++) {
+
+    
+
+    console.log(allButton[i].innerHTML);
+}
---
+++ getClientIP/getIP.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+async function getIpClient() {
+    try {
+        const response = await axios.get('https://ipinfo.io/json');
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getIpClient();
